4485b06b180d730ff6667c1f8e6ef9c2d15f54d3
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-12 14:16:51 +0200

    05 done

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,47 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const currParent = curr.parentElement
+
+// #1
+const newButton = document.createElement('button')
+newButton.classList.add('article__btn')
+newButton.innerText = 'usuń z koszyka'
+currParent.appendChild(newButton)
+
+// #2
+
+const siblingElements = currParent.children
+const siblingsArray = [...siblingElements]
+
+siblingsArray.forEach(function(element){
+    if(element.getAttribute('class').includes('js-curr')) return 
+
+    element.classList.add('siblings')
+})
+
+// #3
+
+const nextArticleElement = currParent.nextElementSibling
+
+nextArticleElement.setAttribute('title', 'nextElementSibling')
+
+
+// #4 
+
+const lastArticleElement = nextArticleElement.nextElementSibling
+
+const newParagraphElement = document.createElement('p')
+newParagraphElement.innerText = 'new paragraph'
+
+const lastArticleButtonElement = lastArticleElement.querySelector('button')
+
+lastArticleElement.insertBefore(newParagraphElement, lastArticleButtonElement)
+
+// #5
+
+const clonedArticleElement = nextArticleElement.cloneNode(true)
+const sectionElement = currParent.parentElement
+
+sectionElement.insertBefore(clonedArticleElement, currParent)
