ad4bb4640befc57903588e6a62e8a96818746245
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-12 12:45:40 +0200

    01 done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,5 @@
 console.log('DOM');
+
+const liElement = document.querySelector('.comments__item.comments__item--newest')
+const spanDataElements = liElement.querySelectorAll('span[data-info]')
+console.log(spanDataElements.length);
