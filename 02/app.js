b83f05051b485e43f079f7e0e92f7d3c856ac5be
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-12 12:53:11 +0200

    02 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const linkElements = document.querySelectorAll('a[data-url]')
+
+linkElements.forEach(function(element){
+    const data = element.getAttribute('data-url')
+    element.setAttribute('href', data)
+})
